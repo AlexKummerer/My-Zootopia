c0549dc3d1803a56ffb1c4e34ac16fdc2d24f1f2
alexkummerer <alexander@thomas-kummerer.de>
2024-06-27 17:10:45 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,40 +1,69 @@
 import json
 
+
 def load_data(file_path):
     """Loads a JSON file"""
-    with open(file_path, 'r') as handle :
+    with open(file_path, "r") as handle:
         return json.load(handle)
 
 
-def print_animal_information(animal):
-    """Check and print animal information"""
-    if 'name' in animal:
-        print('\nName:', animal['name'])
-    
-    if 'characteristics' in animal:
-        characteristics = animal['characteristics']
-        if 'diet' in characteristics:
-            print('Diet:', characteristics['diet'])
-        if 'type' in characteristics:
-            print('Type:', characteristics['type'])
-    
-    if 'locations' in animal and animal['locations']:
-        print('Location:', animal['locations'][0])
+def read_template(template_path):
+    """Read HTML template content"""
+    with open(template_path, "r") as file:
+        return file.read()
 
 
-def print_animals_data(animals_data):
-    """print all animals information"""
-    
+def write_html(file_path, content):
+    """Write content to an HTML file"""
+    with open(file_path, "w") as file:
+        file.write(content)
+
+
+def generate_animal_information(animal):
+    """Generate animal information string"""
+    info = ""
+    if "name" in animal:
+        info += f"Name: {animal['name']}\n"
+
+    if "characteristics" in animal:
+        characteristics = animal["characteristics"]
+        if "diet" in characteristics:
+            info += f"Diet: {characteristics['diet']}\n"
+        if "type" in characteristics:
+            info += f"Type: {characteristics['type']}\n"
+
+    if "locations" in animal and animal["locations"]:
+        info += f"Location: {animal['locations'][0]}\n"
+
+    info += "\n"
+    return info
+
+
+def generate_animals_data_string(animals_data):
+    """Generate string with all animals information"""
+    output = ""
     for animal in animals_data:
-        print(animal)
-        print_animal_information(animal)
+        output += generate_animal_information(animal)
+    return output
+
+
+def replace_placeholder(template_content, placeholder, replacement):
+    """Replace placeholder in template with the given replacement"""
+    return template_content.replace(placeholder, replacement)
 
 
 def main():
     animals_data = load_data("animals_data.json")
+    template_content = read_template("animals_template.html")
 
-    print_animals_data(animals_data)
+    animals_info_string = generate_animals_data_string(animals_data)
+    updated_content = replace_placeholder(
+        template_content, "__REPLACE_ANIMALS_INFO__", animals_info_string
+    )
+
+    write_html("animals.html", updated_content)
+    print("New HTML content written to animals.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
